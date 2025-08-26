1e5e7599dfdb1342172b5a5d0245799a47e24838
delivelGD <prostofilin2@gmail.com>
2025-08-26 14:31:52 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+#include <filesystem>
 
 using namespace geode::prelude;
 
@@ -9,7 +10,17 @@ class $modify(PlayLayer) {
         auto ret = PlayLayer::createCheckpoint();
         if (!ret) return nullptr;
 
+        auto customFile1 = Mod::get()->getSettingValue<std::filesystem::path>("choose-custom-sound-for-creating");
+        auto enableCSounds = Mod::get()->getSettingValue<bool>("enable-creating-sounds");
+        if (customFile1 == "YourSound" && enableCSounds == true) {
+
         FMODAudioEngine::sharedEngine()->playEffect("checkpointSound.ogg"_spr);
+        }
+        else {
+            if (enableCSounds == true) {
+                FMODAudioEngine::sharedEngine()->playEffect(customFile1.string().c_str());
+            }
+        }
 
         return ret;
     }
@@ -18,6 +29,16 @@ class $modify(PlayLayer) {
 
         PlayLayer::removeCheckpoint(p0);
 
+        auto customFile2 = Mod::get()->getSettingValue<std::filesystem::path>("choose-custom-sound-for-deleting");
+        auto enableDSounds = Mod::get()->getSettingValue<bool>("enable-deleting-sounds");
+        if (customFile2 == "YourSound" && enableDSounds == true) {
+
         FMODAudioEngine::sharedEngine()->playEffect("checkpointSound1.ogg"_spr);
+        }
+        else {
+            if (enableDSounds == true) {
+                FMODAudioEngine::sharedEngine()->playEffect(customFile2.string().c_str());
+            }
+        }
     }
 };
